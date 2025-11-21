b10691c945014b53b1dee275fdb038ef043c0456
Joseph Varghese <josephvarghese98128@gmail.com>
2025-11-21 15:20:09 +0530

    Cart & Place Order

---
+++ backend/services/order_service.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+from models.order_model import Order
+import json
+
+class OrderService:
+
+    @staticmethod
+    def create_order(db: Session, product_ids: list):
+        csv_data = ",".join([str(pid) for pid in product_ids])
+        order = Order(product_ids=csv_data)
+        db.add(order)
+        db.commit()
+        db.refresh(order)
+        return order
